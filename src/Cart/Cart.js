4a78b2e64827ac3e0aaef6078e7a708748df3473
banalaarjunreddy <bareddy0925@gmail.com>
2024-07-11 06:29:49 +0530

    react extentions

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import './Cart.css'
 
 export default function Cart() {
     const userId=localStorage.getItem("userId")
     const [cartItems,setCartItems]=useState([])
     const [loading,setLoading]=useState(true)
     useEffect(()=>{
-        getCartProducts()
-    },[])
+        getCartProducts()},[])
     async function getCartProducts(){
         const response=await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/cart?userId=${userId}`)
         setCartItems(response.data.items)
@@ -20,15 +20,17 @@ export default function Cart() {
                 loading? 
                     <>Loading...</>
                 :(
-                    <div className='cart-items'>
+                    <div className=' cart-items'>
                         {
                             cartItems.map((item)=>(
+                                <div className='cart-items1'>
                                 <div key={item.product._id}>
                                     <h3>{item.product.name}</h3>
+                                    <p><b>Price : </b>{item.product.price}</p>
                                     <p>{item.product.description}</p>
-                                    <p>{item.product.price}</p>
                                     <p>{item.product.category}</p>
                                     <p>Qunatity:{item.quantity}</p>
+                                </div>
                                 </div>
                             ))
                         }
